da1a727972ef5bf63730cce970cb9beec2898385
meenu <meenu.rana@successive.tech>
2021-04-26 17:37:27 +0530

    Variables - let, const, var

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
-// alert('123');
+// LET CONST VAR
 
-console.log('hello world');
-console.log(123);
-console.table([1, 2, 3, 4, 5, 6]);
-console.table({ a: 2, b: 1 });
-var greeting = 'hello user';
-console.log(greeting);
-console.error('please enter correct error');
-console.clear();
-console.warn('This is warning');
+// var nme = 'John Doe';
+// console.log(nme);
+// nme = 'brad traversy';
+// console.log(nme);
+
+// // Var INIT
+
+// var greeting;
+// console.log(greeting);
+// greeting = '123';
+// console.log(greeting);
+
+// // var can be write letters, Numbers, _underscore, $moneysign
+// // Cannot start with number
+
+// var working = 'No';
+// console.log(working);
+
+// var _working = 'no';
+// console.log(_working);
+
+// var $working = 'No';
+// console.log($working);
+
+// // var 1working = "no";
+// // console.log(1working);
+
+// // Multi word Var
+
+// var firstName = 'john Doe'; //Camel case
+// var first_name = 'sara'; //Underscore
+// var FirstName = 'don jay'; //Pascal case
+// console.log(FirstName);
+
+// // LET
+// let nme;
+// nme = 'John Doe';
+// console.log(nme);
+// nme = 'brad traversy';
+// console.log(nme);
+
+// CONST
+
+const nme = 'john doe';
+// nme = 'sara'; //cannot re-assign to const
+console.log(nme);
+
+const address = {
+  house_no: 50,
+  location: 'delhi',
+  country: 'india',
+};
+
+address.location = 'mumbai'; //Reassign value in it. but not that variable name
+console.log(address);
+
+const number = [1, 2, 3, 4, 5, 6];
+number.push(7);
+
+console.log(number);
