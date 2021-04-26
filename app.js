36268706a16c4022478cf05cce48c8665ea9e1d7
meenu <meenu.rana@successive.tech>
2021-04-26 10:49:18 +0530

    Using console

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('123');
+// alert('123');
+
+console.log('hello world');
+console.log(123);
+console.table([1, 2, 3, 4, 5, 6]);
+console.table({ a: 2, b: 1 });
+var greeting = 'hello user';
+console.log(greeting);
+console.error('please enter correct error');
+console.clear();
+console.warn('This is warning');
