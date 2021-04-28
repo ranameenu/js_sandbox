00d3afd8ae7b09ec19aef489424d0b3c64d39604
meenu <meenu.rana@successive.tech>
2021-04-28 23:49:02 +0530

    Number & math object

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-// PRIMITIVE TYPE
+// Numbers & math object
 
-// String
-const name = 'John Doe';
-// Numbers
-const age = 30;
-// Boolean
-const hasKids = false;
-// Null
-const car = null;
-// Undefined
-let salary;
-// Symbol
-const sym = Symbol();
+const num1 = 130;
+const num2 = 30;
+let val;
 
-// REFERENCE TYPE
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// Array
-const hobbies = ['movies', 'music', 'swim'];
-// Object Literals
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.3);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-6);
+val = Math.pow(8, 3);
+val = Math.min(5, 7, 89, 25, 43, 0, 8, 65);
+val = Math.max(5, 7, 89, 25, 43, 0, 8, 65);
+val = Math.ceil(200000 * Math.random());
 
-const emp = {
-  name: 'meenu',
-  working: 'front-end',
-};
-const today = new Date();
-
-console.log(typeof today);
+console.log(val);
