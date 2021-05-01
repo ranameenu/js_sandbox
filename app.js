3b1bb241615af1702b7e1af745c8b26714f1954c
meenu <meenu.rana@successive.tech>
2021-05-01 18:53:01 +0530

    object literals

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-// Arrays
+const person = {
+  firstName: 'steve',
+  lastName: 'smith',
+  age: 30,
+  email: 'steve@aol.com',
+  hobbies: ['movies', 'music', 'swimming', 'sports'],
+  address: {
+    city: 'Miami',
+    state: 'FL',
+  },
+  getBirthYear: function (current) {
+    return current - this.age;
+  },
+};
 
-const numbers = [45, 24, 65, 12, 9, 0, 78, 33, 19];
-const numbers1 = new Array(45, 24, 65, 12, 9, 0, 78, 33, 19);
-
-const fruits = ['apple', 'mango', 'banana', 'orange'];
-const mixed = ['brad', 20, true, undefined, null, { a: 1, b: 2 }, new Date()];
 let val;
 
-// GET ARRAY LENGTH
-val = numbers.length;
+val = person.firstName;
+val = person['firstName'];
+val = person.age;
+val = person.hobbies;
+val = person.address;
+val = person.getBirthYear('1988');
+val = person.hobbies[1];
+val = person.address.city;
+val = person.getBirthYear('2021');
 
-// CHECK IF IS ARRAY
-val = Array.isArray(numbers);
-
-// TO GET SINGLE VALUE FROM ARRAY
-val = numbers[3];
-
-// TO INSERT SOMETHING IN ARRAY
-numbers[2] = 20;
-
-// FIND INDEX OF THE VALUE
-val = numbers.indexOf(33);
-
-// Mutating Arrays
-// Add value at the end
-numbers.push(20);
-
-// Add value at the front
-numbers.unshift(44);
-
-// Pop from the end
-numbers.pop();
-
-// pop from the front
-numbers.shift();
-
-// Splice Values()
-numbers.splice(1, 3); //to remove values from to to
-
-// reverse()
-numbers.reverse();
-
-// concatenate arrays
-val = numbers.concat(numbers1);
-
-// Sorting arrays
-val = fruits.sort();
-val = numbers.sort();
-console.log(numbers);
 console.log(val);
+
+const people = [
+  {
+    name: 'mike',
+    age: 29,
+    job: 'developer',
+  },
+  {
+    name: 'john',
+    age: 35,
+    job: 'front-end-developer',
+  },
+  {
+    name: 'brad',
+    age: 36,
+    job: 'engineer',
+  },
+];
+
+for (i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
