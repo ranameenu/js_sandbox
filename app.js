aaf9a318df240a4fcf8f34b2f908a0ff121ca1d8
meenu <meenu.rana@successive.tech>
2021-05-01 16:59:55 +0530

    array and array methods

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-// Template literals
+// Arrays
 
-var name = 'john';
-var age = 20;
-var job = 'developer';
-var city = 'delhi';
-let html;
+const numbers = [45, 24, 65, 12, 9, 0, 78, 33, 19];
+const numbers1 = new Array(45, 24, 65, 12, 9, 0, 78, 33, 19);
 
-// Without template literals (es5)
-html =
-  '<ul><li>Name: ' +
-  name +
-  '</li><li>Age: ' +
-  age +
-  '</li><li>Job: ' +
-  job +
-  '</li><li>City: ' +
-  city +
-  '</li></ul>';
+const fruits = ['apple', 'mango', 'banana', 'orange'];
+const mixed = ['brad', 20, true, undefined, null, { a: 1, b: 2 }, new Date()];
+let val;
 
-//   With template literals (es6)
-function hello(instructor) {
-  return instructor;
-}
+// GET ARRAY LENGTH
+val = numbers.length;
 
-html = `
-<ul>
-<li>Name:  ${name} </li>
-<li>Age:  ${age}</li>
-<li>Job: ${job}</li>
-<li>City: ${city}</li>
-<li> ${4 + 4}</li>
-<li> ${hello('brad')}</li>
-<li> ${age > 30 ? 'you are old' : 'you are so young'}</li>
-</ul>`;
+// CHECK IF IS ARRAY
+val = Array.isArray(numbers);
 
-document.body.innerHTML = html;
+// TO GET SINGLE VALUE FROM ARRAY
+val = numbers[3];
+
+// TO INSERT SOMETHING IN ARRAY
+numbers[2] = 20;
+
+// FIND INDEX OF THE VALUE
+val = numbers.indexOf(33);
+
+// Mutating Arrays
+// Add value at the end
+numbers.push(20);
+
+// Add value at the front
+numbers.unshift(44);
+
+// Pop from the end
+numbers.pop();
+
+// pop from the front
+numbers.shift();
+
+// Splice Values()
+numbers.splice(1, 3); //to remove values from to to
+
+// reverse()
+numbers.reverse();
+
+// concatenate arrays
+val = numbers.concat(numbers1);
+
+// Sorting arrays
+val = fruits.sort();
+val = numbers.sort();
+console.log(numbers);
+console.log(val);
