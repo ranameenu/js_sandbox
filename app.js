422e6341b16846b7e5b7939198b3e21eb2cc7201
meenu <meenu.rana@successive.tech>
2021-05-10 22:33:06 +0530

    Scope : Let & Const

--- app.js
+++ app.js
@@ -1,62 +1,25 @@
-// WINDOWS OBJECT / METHOD / PROPERTIES
+var a = 1;
+let b = 2;
+const c = 3;
 
-// // Alert
-// alert(123);
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
 
-// // Prompt
-// prompt(); without the alert following prompt
-
-// // Prompt with Alert
-// const input = prompt();
-// alert(input);
-
-// // Confirm
-
-// if (confirm('Are you sure')) {
-//   console.log('yes');
-// } else {
-//   console.log('no');
+//   console.log(
+//     `function Scope of variable a, b, c are respectively ${a}, ${b} & ${c} `
+//   );
 // }
+// test();
 
-let val;
+if (true) {
+  var a = 7;
+  let b = 8;
+  const c = 9;
+  console.log(`Block Scope ${a}, ${b}, ${c}`);
+}
 
-// Outer width & height
-val = window.outerHeight;
-val = window.outerWidth;
-
-// Inner Height & Width
-val = window.innerHeight;
-val = window.innerWidth;
-
-// Scroll Points
-val = window.scrollY;
-val = window.scrollX;
-
-// Location Object
-val = window.location;
-val = window.location.hostname;
-val = window.location.pathname;
-val = window.location.origin;
-val = window.location.port;
-val = window.location.protocol;
-
-// // Redirect
-// window.location.href = 'https://google.com'; // It will redirect your web page to that URL
-
-// // Reload
-// window.location.reload(); It will continuously reload our window
-
-// // History
-// window.history.go(-1);
-val = window.history.length;
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.appCodeName;
-val = window.navigator.appName;
-val = window.navigator.onLine;
-val = window.navigator.cookieEnabled;
-val = window.navigator.platform;
-val = window.navigator.userAgent;
-val = window.navigator.geolocation;
-console.log(val);
+console.log(
+  `Global Scope of variable a, b, c are respectively ${a}, ${b} & ${c} `
+);
